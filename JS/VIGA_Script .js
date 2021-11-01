9ad3f6715f474371db9b2eb09ef1171ac0eff464
VIGAKING <54349595+VIGAKING@users.noreply.github.com>
2021-11-01 10:44:57 +0800

    Update and rename 10000_cookie.js to VIGA_Script .js

---
+++ JS/VIGA_Script .js
@@ -0,0 +1,23 @@
+
+# 订阅地址 https://raw.githubusercontent.com/VIGAKING/Quantumult-X/main/JS/Js_VIGA.conf
+
+
+hostname = api.m.jd.com,trade-acs.m.taobao.com,api.weibo.cn, mapi.weibo.com, *.uve.weibo.com
+
+
+
+# 京东历史价格
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
+
+
+# 淘宝历史价格
+# 不生效或失效的需要卸载 tb 重装，注意不开脚本进 tb 会失效
+^http://.+/amdc/mobileDispatch url script-request-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js
+^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js
+
+
+# 微博去广告
+^https?://(sdk|wb)app\.uve\.weibo\.com(/interface/sdk/sdkad.php|/wbapplua/wbpullad.lua) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/wb_launch.js
+^https?://m?api\.weibo\.c(n|om)/2/(statuses/(unread|extend|positives/get|(friends|video)(/|_)(mix)?timeline)|stories/(video_stream|home_list)|(groups|fangle)/timeline|profile/statuses|comments/build_comments|photo/recommend_list|service/picfeed|searchall|cardlist|page|!/(photos/pic_recommend_status|live/media_homelist)|video/tiny_stream_video_list|photo/info|remind/unread_count) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/wb_ad.js
+
+
